f94278cafd89ebfa131a328ad3fb6f759938e0f7
Sangkan-K <gimsanghan53@gmail.com>
2022-07-29 23:12:51 +0900

    220729

---
+++ TS Challenge/Classes.ts
@@ -0,0 +1,9 @@
+class Player {
+    constructor(
+        private firstName:string,
+        private lastName:string
+        public nickname:string
+    ) {}
+}
+
+const nico = new Player("nice", "las", "니꼬")
