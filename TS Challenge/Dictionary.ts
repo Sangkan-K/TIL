f225255d027456b723a8bf3c82320fc99d11a003
Sangkan-K <gimsanghan53@gmail.com>
2022-07-31 00:40:27 +0900

    220730

--- TS Challenge/Dictionary.ts
+++ TS Challenge/Dictionary.ts
@@ -32,3 +32,63 @@ const dict = new Dict();
 
 dict.add(kimchi);
 dict.def("kimchi");
+
+// 과제 & 실습
+
+// type Words = {
+//     [key: string]: string;
+//   };
+
+//   class Dict {
+//     private dictionary: Words;
+//     constructor() {
+//       this.dictionary = {};
+//     }
+//     add(word: Word) {
+//       if (this.dictionary[word.term] === undefined) {
+//         this.dictionary[word.term] = word.def;
+//       }
+//     }
+//     get(term: string) {
+//       return this.dictionary[term];
+//     }
+//     delete(term: string) {
+//       delete this.dictionary[term]
+//     }
+//     update(word: Word) {
+//         if (this.dictionary[word.term]) {
+//         this.dictionary[word.term] = word.def
+//         }
+//     }
+//     showAll() {
+//       console.log(Object.keys(this.dictionary))
+//     }
+//     count() {
+//         const dictlength = Object.keys(this.dictionary).length
+//         console.log(dictlength)
+//     }
+
+//   }
+
+//   class Word {
+//     constructor(public term: string, public def: string) {}
+//   }
+
+// const kimchi = new Word("kimchi", "한국의 음식");
+// const rice = new Word("rice", "한국의 주식");
+
+// const dict = new Dict();
+
+// dict.add(kimchi)
+// dict.add(rice)
+// console.log(dict)
+// console.log(dict.get("kimchi"))
+
+// const newRice = new Word("rice" , "밥없인 못살아")
+
+// dict.update(newRice)
+
+// console.log(dict)
+
+// dict.showAll()
+// dict.count()
