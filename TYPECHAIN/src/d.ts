1dab1d490cf29ba3ae818f4a89943d2851e77b34
Sangkan-K <gimsanghan53@gmail.com>
2022-08-07 04:41:44 +0900

    220807

---
+++ TYPECHAIN/src/d.ts
@@ -0,0 +1,11 @@
+// declare module "lodash" {
+//     function head<T>(array: T[]): T | undefined
+//     function hasIn(object: object, key: string): boolean
+//     function isBoolean(value : any): boolean
+//     function toString(value : any): string
+//     function split(string:string, separator: RegExp | string, limit: number): string[]
+//     function hasPath<M>(object: object, path : M[] | string) : boolean
+//     function filter<K>(array:K[], predicate: Function): any[]
+//     function every<L>(array:L[], predicate: Function): boolean
+//     function map<N>(array:N[], iteratee:Function): any[]
+//     }
